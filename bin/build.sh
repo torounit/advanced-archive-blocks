2fb63aa0cc8f37ddd4b46338f6c8fc60f65655fc
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2019-01-05 21:25:04 +0900

    fix package.json version

--- bin/build.sh
+++ bin/build.sh
@@ -5,3 +5,4 @@ npm run build
 
 sed -i -e "s/nightly/${TRAVIS_TAG}/" $(basename $TRAVIS_REPO_SLUG).php
 sed -i -e "s/nightly/${TRAVIS_TAG}/" readme.txt
+sed -i -e "s/nightly/${TRAVIS_TAG}/" package.json
